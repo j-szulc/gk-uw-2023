c3dbb3d6411a4ba0bba303372ca43f47b4638d75
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2023-01-23 23:56:53 +0100

    Add multiple spheres to the scene

--- scene.py
+++ scene.py
@@ -8,11 +8,12 @@ class Scene:
     def __init__(self):
         material = BasicMaterial(ambient_color=np.array([0.5, 0, 0]), diffuse_color=np.array([0.5, 0, 0]),
                                      specular_color=np.array([1, 1, 1]), shininess=25)
-        self.primitives = [
-            Sphere(center=np.array([0, 0, 3]), radius=1, material=material),
-            Sphere(center=np.array([2, 2, 3]), radius=1, material=material)
-        ]
-        self.lights = [np.array([5, 5, 3])]
+
+        self.primitives = []
+        for i in range(10):
+            for j in range(10):
+                self.primitives.append(Sphere(center=np.array([i, j, 3]), radius=0.5, material=material))
+        self.lights = [np.array([0.5, 0.5, 2.9])]
 
     def render_rays(self, rays):
         width = 600
